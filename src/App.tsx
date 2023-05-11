61bda3009e57ddcf76bf5b9bc5c0469c2fd5b7ef
Deeptaneel Dey <deeptaneel.dey111@gmail.com>
2023-05-12 01:45:36 +0530

    Tic tac toe inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styles from "./styles/Home.module.css"
+import Board from "./containers/Board";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      
+
+      <main className={styles.main}>
+        <Board />
+      </main>
     </div>
   );
 }
